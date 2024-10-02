32af618258cd26d1c4ab2d06c2f6476e05b9fcd6
GIOVANNA COELHO GILIO DE SA <=>
2024-10-02 10:38:09 -0300

    Listas

--- AULA--06/modulo/funcoes.js
+++ AULA--06/modulo/funcoes.js
@@ -69,4 +69,78 @@ const comandosBasicos = function (){
     console.log(listaDeProdutos.lengtah)
 }
 
-comandosBasicos()
+const percorrerArray = function(){
+    let nomesDeAlunos = ['Eduarda de Jesus', 
+                        'Felipe Vieira',
+                        'Luiz Henrique',
+                        'Gabriel Guedes',
+                        'Sofia Souza',
+                        'Giovanna Gilio',
+                        'Beatriz Coelho',
+                        'Renato Augusto',
+                        'Eduardo Nascimento',
+                    ]
+
+    let contador = 0
+    // noemesDeAlunos.length -> retorna quantos elementos temos no array 
+    console.log('EXEMPLO DO WHILE')
+    while(contador < nomesDeAlunos.length){
+        console.log (nomesDeAlunos[contador])
+        contador += 1
+    }
+    
+    console.log(`EXEMPLO DO FOR`)
+    for(let contador = 0; contador < nomesDeAlunos.length; contador ++){
+        console.log (nomesDeAlunos[contador])
+    }
+
+    console.log ('EXEMPLO forEach')
+    nomesDeAlunos.forEach(function(item){
+        console.log (item)
+    })
+}
+
+const filtrarProduto = function(nomeProduto){
+    let produto = String(nomeProduto).toUpperCase()
+    let status = false
+    let listaDeProdutos = []
+    listaDeProdutos.push('Teclado',
+                        'Mouse',
+                        'Caixa de Som',
+                        'Impressora',
+                        'Monitor',
+                        'Gabinete',
+                        'Memória',
+                        'HD',
+                        'SSD',
+                        'Placa de Video',
+                        'Placa Mãe'
+                        )
+
+    // Maneira tras com que ao pesquisar a palavra precisa ser totalmente igual, pensando em um front-end
+    listaDeProdutos.forEach(function(item){
+        if(String(item).toUpperCase() == produto){
+            status = true
+        }
+    })
+
+    // Maneira tras com que ao pesquisar a palavra pode aparecer varios itens, por exemplo placas, pensando em um front-end
+    //includes() -> Permite verificar se existe um conteudo dentro do array(retorna true ou false)
+    listaDeProdutos.forEach(function(item){
+        if(String(item).toUpperCase().includes(produto)){
+            status = true
+        }
+    })
+    
+    //indexOf() -> Permite verificar se existe um conteudo dentro do array (retorn 0 OU -1)
+    listaDeProdutos.forEach(function(item){
+        if(String(item).toUpperCase().indexOf(produto) == 0){
+        status = true
+        }
+    })
+    return status
+}
+
+console.log(filtrarProduto('placa'))
+//percorrerArray()
+//comandosBasicos()
